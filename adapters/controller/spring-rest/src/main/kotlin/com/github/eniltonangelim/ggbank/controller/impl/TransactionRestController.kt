b06812fee6f2c073baea58bc06f497e975abe1f4
Angelim <enilton.angelim@gmail.com>
2021-05-27 00:00:42 -0300

    feat(controller) add entrypoint to transactions

--- adapters/controller/spring-rest/src/main/kotlin/com/github/eniltonangelim/ggbank/controller/impl/TransactionRestController.kt
+++ adapters/controller/spring-rest/src/main/kotlin/com/github/eniltonangelim/ggbank/controller/impl/TransactionRestController.kt
@@ -1,6 +1,24 @@
 package com.github.eniltonangelim.ggbank.controller.impl
 
+import com.github.eniltonangelim.ggbank.controller.TransactionController
+import com.github.eniltonangelim.ggbank.usecases.transaction.TransactionUsecase
+import com.github.eniltonangelim.ggbank.usecases.transaction.dto.RegisterTransactionRequestDTO
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 
-class TransactionControllerImpl {
+@RestController
+@RequestMapping(TransactionRestController.PATH)
+class TransactionRestController(val transactionUsecase: TransactionUsecase) : TransactionController {
+
+    companion object {
+        internal const val PATH = "/transaction"
+    }
+
+    @PostMapping
+    override fun transfer(transactionRequestDTO: RegisterTransactionRequestDTO) {
+        transactionUsecase.transfer(transactionRequestDTO)
+    }
+
 }
